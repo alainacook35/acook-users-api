7f2e29bebe00d3fc66cfb46bc5b54b6aa4f368e4
Alaina Cook <alaina.leeann35@gmail.com>
2026-09-02 15:46:10 -0400

    project setup

---
+++ src/main/java/com/acook/magmutualusersapi/AcookUsersApiApplication.java
@@ -0,0 +1,13 @@
+package com.acook.magmutualusersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcookUsersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcookUsersApiApplication.class, args);
+    }
+
+}
